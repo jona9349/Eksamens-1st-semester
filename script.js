c02ebba313e51f5a29c0a6f3d8e8e6493787c2fc
Jonas Paulsen <jonas@Jonass-MacBook-Pro-2.local>
2019-06-19 00:51:34 +0200

    Færdig bum og valideret! Godnat

---
+++ script.js
@@ -0,0 +1,19 @@
+function knap1() {
+    location.replace("http://jonaspaulsen.dk/kea/01-web/02-html/01_02_03_stilartsite_v2");
+}
+
+function knap2() {
+    location.replace("http://jonaspaulsen.dk/kea/02-animation/spil/");
+}
+
+function knap3() {
+    location.replace("https://www.juliegunner.com/kea/03-content/03-redesign/03_02_08_redesign/index.html");
+}
+
+function knap4() {
+    location.replace("https://youtu.be/gT8UlLrD704");
+}
+
+function knap5() {
+    location.replace("https://xd.adobe.com/view/386ec4dc-0cf0-4244-780c-718929a6250d-4e5f/?fbclid=IwAR1F6qqdLg0Pqh__0kwiwSmUk87exujcZg1xau7eYX-EJ35dSZFeUTKnKpA&fullscreen");
+}
